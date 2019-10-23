7a850f93d73e6439cbb443151e3c81ed47c510d7
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-10-23 20:19:49 -0300

    Adição de métodos de interface com os atributos de Mesa (Pote e vector de Carta)

--- include/classes/mesa.hpp
+++ include/classes/mesa.hpp
@@ -1,14 +1,24 @@
 #include <vector>
-#ifndef MESA
-#define MESA
+#include <classes/carta.hpp>
+#include <classes/pote.hpp>
+
+#ifndef MESA_H
+#define MESA_H
 
 class Mesa{
 	private:
 		std::vector<Carta> cartasMesa;
-		Pote* pote = new Pote;
+		Pote pote;
+
 	public:
 		Mesa();
 		void addCartas(std::vector<Carta> cartasMesa);
+		int getPoteTotal();
+		int getPoteAtual();
+		int getPoteAnterior();
+		int setPoteTotal();
+		int setPoteAtual();
+		int setPoteAnterior();
 };
 
 #endif
--- src/classes/mesa.cpp
+++ src/classes/mesa.cpp
@@ -1,8 +1,9 @@
-#include "classes/mesa.hpp"
+#include <classes/mesa.hpp>
 #include <vector>
 
-Mesa::Mesa(){ //TODO: Construtor do objeto Mesa
-
+//TODO: Definir corretamente os parâmetros do construtor do Pote
+Mesa::Mesa(){
+    this->pote = Pote();
 }
 
 void Mesa::addCartas(std::vector<Carta> cartasMesa){
@@ -12,4 +13,28 @@ void Mesa::addCartas(std::vector<Carta> cartasMesa){
         //Exceção de excesso de cartas
 }
 
+//TODO: Setters e Getters de Pote
 
+int Mesa::getPoteAnterior(){
+    return(0);
+}
+
+int Mesa::getPoteAtual(){
+    return(0);
+}
+
+int Mesa::getPoteTotal(){
+    return(0);
+}
+
+int Mesa::setPoteAnterior(){
+    return;
+}
+
+int Mesa::setPoteAtual(){
+    return;
+}
+
+int Mesa::setPoteTotal(){
+    return;
+}

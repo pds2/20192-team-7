65d2e1f2731beb5263713bcb039e903d2f8dc430
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-10-23 11:07:51 -0300

    Início da implementação dos métodos no arquivo Mesa.cpp

--- src/classes/mesa.cpp
+++ src/classes/mesa.cpp
@@ -1,2 +1,15 @@
-#include "mesa.hpp"
+#include "classes/mesa.hpp"
+#include <vector>
+
+Mesa::Mesa(){ //TODO: Construtor do objeto Mesa
+
+}
+
+void Mesa::addCartas(std::vector<Carta> cartasMesa){
+    if (cartasMesa.size() <= 5)
+        this->cartasMesa = cartasMesa;
+    else
+        //Exceção de excesso de cartas
+}
+
 

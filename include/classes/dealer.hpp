4f372ad51788100a371b27f4440dabbf96c54703
Bruno Silva <brunosife@gmail.com>
2019-11-20 20:34:49 -0300

    Começo desenvolvimento classe dealer (#10)

--- include/classes/dealer.hpp
+++ include/classes/dealer.hpp
@@ -1,7 +1,26 @@
-#ifndef DEALER
-#define DEALER
-namespace poker{
-    class Dealer{
+#include "etapajogo.hpp"
+
+#ifndef POKER_BARALHO
+#define POKER_BARALHO
+
+namespace poker {
+    
+    class Dealer {
+        private:
+            unsigned int numeroJogadores;
+            EtapaJogo momentoJogo;
+            
+        public:
+            Dealer();
+
+            Dealer(EtapaJogo momentoJogo, unsigned int numeroJogadores);
+            
+            void setEtapaJogo();
+            void setNumeroJogadores();
+            
+            EtapaJogo getMomentoJogo();
+            int getNumeroJogadores();
+
     };
 }
 #endif

3d977b1486f93ed396010ff7dc1e177025fe45f8
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-11-21 20:10:39 -0300

    Testes da classe Pote implementados

--- src/tests/testPote.cpp
+++ src/tests/testPote.cpp
@@ -1,4 +1,34 @@
 #include <test/doctest.h>
 #include <classes/Pote.cpp>
+using namespace poker;
 
-TEST_CASE
+TEST_CASE("Testando Construtor da classe Pote"){
+    CHECK_NOTHROW(Pote());
+    CHECK_NOTHROW(Pote(5,5,5));
+    CHECK_THROWS(Pote('a',1,2));
+    CHECK_THROWS(Pote('@', 2, -999.5));
+}
+
+TEST_CASE("Testando Setters"){
+    Pote poteTeste;
+    CHECK_NOTHROW_MESSAGE(poteTeste.setValorApostaAnterior(1), "setValorApostaAnterior OK!");
+    CHECK_NOTHROW_MESSAGE(poteTeste.setValorTotal(1), "setValorApostaTotal OK!");
+    CHECK_NOTHROW_MESSAGE(poteTeste.setValorApostaAtual(1), "setValorApostaTotal OK!");
+}
+
+TEST_CASE("Testando Getters"){
+    Pote poteTeste(5,5,5);
+    CHECK_EQ(poteTeste.getValorApostaAnterior(), 5);
+    CHECK_EQ(poteTeste.getValorApostaAtual(), 5);
+    CHECK_EQ(poteTeste.getValorTotal(), 5);
+}
+
+TEST_CASE("Testando Setters e Getters"){
+    Pote poteTeste;
+    poteTeste.setValorApostaAnterior(2);
+    poteTeste.setValorApostaAtual(7);
+    poteTeste.setValorTotal(9);  
+    CHECK_EQ(poteTeste.getValorApostaAnterior(), 2);
+    CHECK_EQ(poteTeste.getValorApostaAtual(), 7);
+    CHECK_EQ(poteTeste.getValorTotal(), 9);  
+}

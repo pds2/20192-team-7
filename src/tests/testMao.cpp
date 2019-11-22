b66104c49ceb4b146b5db95b3de82c08f5ede4ad
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-11-22 19:11:16 -0300

    Correções nos testes da classe Mão

--- src/tests/testMao.cpp
+++ src/tests/testMao.cpp
@@ -1,34 +1,28 @@
-#include <include/classes/Mao.hpp>
-#include <include/test/doctest.h>
+#include <classes/mao.hpp>
+#include <test/doctest.h>
 
 using namespace poker;
 
 TEST_CASE("Testando o construtor de Mao"){
+	Carta cartaTeste1(Paus, Q);
+	Carta cartaTeste2(Copas, Q);
+	Carta cartaTeste3(Ouros, Q);
+	Carta cartaTeste4(Espadas, Q;
+	Carta cartaTeste5(Paus, QUATRO);
+	Carta cartaTeste6(Copas, SETE);
 
-	Carta cartaTeste1(0, 0);
-	Carta cartaTeste2(0, 0);
-	Carta cartaTeste3(3, 6);
-	Carta cartaTeste4(2, 8);
-	Carta cartaTeste5(1, 3);
-	Carta cartaTeste6(4, 4);
-
-	CHECK_THROWS(maoTeste1(cartaTeste1, cartaTeste2));
-	CHECK_THROWS(maoTeste2(12, 3));
-
+	CHECK_NOTHROW(maoTeste1(cartaTeste1, cartaTeste2));
 	CHECK_NOTHROWS(maoTeste3(cartaTeste3, cartaTeste4));
 	CHECK_NOTHROWS(maoTeste4(cartaTeste5, cartaTeste6));
 }
 
 TEST_CASE("Testando getCartas"){
 
-	std::vector<Carta>teste1;
-
-	Carta cartaTeste7(1, 3);
-	Carta cartaTeste8(4, 4);
-
+	std::vector<Carta> teste1;
+	Carta cartaTeste7(Copas, Q);
+	Carta cartaTeste8(Paus, Q);
 	teste1.pushback(cartaTeste7);
 	teste1.pushback(cartaTeste8);
-
 	Mao maoTeste5(cartaTeste7, cartaTeste8);
 
 	CHECK_EQ(maoTeste5.getCartas(), teste1);

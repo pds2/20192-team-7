c27dfa87edaad624d4b5141282cbaa27fb90aaf9
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-11-22 18:33:42 -0300

    Finalização dos testes da classe mesa

--- src/tests/testMesa.cpp
+++ src/tests/testMesa.cpp
@@ -2,33 +2,47 @@
 #include <classes/mesa.hpp>
 using namespace poker;
 
-TEST_CASE("Testando Construtor da classe Mesa"){
-    CHECK_NOTHROW(Pote());
-    CHECK_NOTHROW(Pote(5,5,5));
-    CHECK_THROWS(Pote('a',1,2));
-    CHECK_THROWS(Pote('@', 2, -999.5));
+TEST_CASE("Testando o construtor vazio da classe Mesa"){
+    CHECK_NOTHROW(Mesa());
+}
+
+TEST_CASE("Testando o construtor parametrizado da classe Mesa"){
+    Pote poteTest;
+    CHECK_NOTHROW(Mesa(poteTest));
+}
+
+TEST_CASE("Testando Adição de Cartas"){
+    Mesa mesaTeste;
+    std::vector<Carta> vectorTeste;
+    for (int i=0; i<5; i++){
+        vectorTeste.push_back(Carta(Copas, Q));
+    }
+    CHECK_NOTHROW_MESSAGE(mesaTeste.addCartas(vectorTeste), "Cartas adicionadas a mesa!");
+}
+
+TEST_CASE("Testando Retorno de Cartas"){
+    Mesa mesaTeste;
+    std::vector<Carta> vectorTeste;
+    for (int i=0; i<5; i++){
+        vectorTeste.push_back(Carta(Copas, Q));
+    }
+    mesaTeste.addCartas(vectorTeste);
+    CHECK_EQ(mesaTeste.getCartasMesa(), vectorTeste);
 }
 
 TEST_CASE("Testando Setters"){
-    Pote poteTeste;
-    CHECK_NOTHROW_MESSAGE(poteTeste.setValorApostaAnterior(1), "setValorApostaAnterior OK!");
-    CHECK_NOTHROW_MESSAGE(poteTeste.setValorTotal(1), "setValorApostaTotal OK!");
-    CHECK_NOTHROW_MESSAGE(poteTeste.setValorApostaAtual(1), "setValorApostaTotal OK!");
+    Mesa mesaTeste;
+    CHECK_NOTHROW_MESSAGE(mesaTeste.setPoteApostaTotal(1), "setPoteApostaTotal OK!");
+    CHECK_NOTHROW_MESSAGE(mesaTeste.setPoteApostaAtual(1), "setPoteAposstaAtual OK!");
+    CHECK_NOTHROW_MESSAGE(mesaTeste.setPoteApostaAnterior(1), "setPoteApostaAnterior OK!");
 }
 
 TEST_CASE("Testando Getters"){
-    Pote poteTeste(5,5,5);
-    CHECK_EQ(poteTeste.getValorApostaAnterior(), 5);
-    CHECK_EQ(poteTeste.getValorApostaAtual(), 5);
-    CHECK_EQ(poteTeste.getValorTotal(), 5);
-}
-
-TEST_CASE("Testando Setters e Getters"){
-    Pote poteTeste;
-    poteTeste.setValorApostaAnterior(2);
-    poteTeste.setValorApostaAtual(7);
-    poteTeste.setValorTotal(9);  
-    CHECK_EQ(poteTeste.getValorApostaAnterior(), 2);
-    CHECK_EQ(poteTeste.getValorApostaAtual(), 7);
-    CHECK_EQ(poteTeste.getValorTotal(), 9);  
+    Mesa mesaTeste;
+    mesaTeste.setPoteApostaTotal(1);
+    mesaTeste.setPoteApostaAtual(1);
+    mesaTeste.setPoteApostaAnterior(1);
+    CHECK_EQ(mesaTeste.getPoteTotal(), 1);
+    CHECK_EQ(mesaTeste.getPoteApostaAtual(), 1);
+    CHECK_EQ(mesaTeste.getPoteApostaAnterior(), 1);
 }

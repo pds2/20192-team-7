3a748e669ae69f57b34121eb5f6a3d26f2ab47e1
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-11-23 19:49:28 -0300

    Criação dos primeiros testes da classe dealer

---
+++ src/tests/testDealer.cpp
@@ -0,0 +1,45 @@
+#include <classes/dealer.hpp>
+#include <test/doctest.h>
+
+using namespace poker;
+
+TEST_CASE("Testando o Construtor de Dealer"){
+    CHECK_NOTHROW_MESSAGE(Dealer(), "Construtor nº1 de Dealer OK!");
+    CHECK_NOTHROW_MESSAGE(Dealer(EstadoJogo(), 4), "Construtor nº2 de Dealer OK!");
+}
+
+TEST_CASE("Testando setters e getters"){
+    Dealer dealer;
+    EstadoJogo estadoTeste;
+    CHECK_NOTHROW_MESSAGE(dealer.setEstadoJogo(estadoTeste), "Set estado Jogo OK!");
+    CHECK_NOTHROW_MESSAGE(dealer.setNumeroJogadores(4), "Set numero de jogadores OK!");
+    CHECK_EQ(dealer.getMomentoJogo(), estadoTeste);
+    CHECK_EQ(dealer.getNumeroJogadores(), 4);
+}
+
+TEST_CASE("Testando a inserção de jogadores"){
+    Dealer dealer;
+    CHECK_NOTHROW_MESSAGE(dealer.inserirJogadores(), "Inserir jogadores OK!");
+}
+
+TEST_CASE("Testando distribuir fichas"){
+    Dealer dealer;
+    dealer.inserirJogadores();
+    CHECK_NOTHROW_MESSAGE(dealer.distribuirFichas(2000), "Inserir fichas OK!");
+}
+
+TEST_CASE("Testando iniciar a partida"){
+    Dealer dealer;
+    CHECK_NOTHROW_MESSAGE(dealer.iniciarPartida(4), "Iniciar partida OK!");
+}
+
+TEST_CASE("Testando entregar o prêmio"){
+    Pote poteTeste(20, 20, 0);
+    Pote* pote = &poteTeste;
+    Mesa* mesa;
+    Dealer dealer;
+    Jogador jogadorTeste("NOME", 2000, pote, mesa);
+    Jogador* pointerJogador = &jogadorTeste;
+    CHECK_NOTHROW_MESSAGE(dealer.entregarPremio(pointerJogador), "Entregar prêmio OK!");
+}
+

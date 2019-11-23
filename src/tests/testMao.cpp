8584714f26f6399c77a39ed3906bc0d6ddace8e1
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-11-23 20:04:29 -0300

    Modificações nos tests da classe Mão

--- src/tests/testMao.cpp
+++ src/tests/testMao.cpp
@@ -3,28 +3,14 @@
 
 using namespace poker;
 
-TEST_CASE("Testando o construtor de Mao"){
-
-	Carta cartaTeste1(Paus, Q);
-	Carta cartaTeste2(Copas, Q);
-	Carta cartaTeste3(Ouros, Q);
-	Carta cartaTeste4(Espadas, Q);
-	Carta cartaTeste5(Paus, QUATRO);
-	Carta cartaTeste6(Copas, SETE);
-
-	CHECK_NOTHROW(Mao(cartaTeste1, cartaTeste2));
-	CHECK_NOTHROW(Mao(cartaTeste3, cartaTeste4));
-	CHECK_NOTHROW(Mao(cartaTeste5, cartaTeste6));
+TEST_CASE("Testando os construtores de Mao"){
+	CHECK_NOTHROW_MESSAGE(Mao(Carta(Copas, Q), Carta(Espadas, A)), "Construtor nº1 de Mão OK!");
+	CHECK_NOTHROW_MESSAGE(Mao(), "Construtor nº 2 de Mão OK!");
 }
 
-TEST_CASE("Testando getCartas"){
-
-	std::vector<Carta> teste1;
-	Carta cartaTeste7(Copas, Q);
-	Carta cartaTeste8(Paus, Q);
-	teste1.push_back(cartaTeste7);
-	teste1.push_back(cartaTeste8);
-	Mao maoTeste5(cartaTeste7, cartaTeste8);
-
-	CHECK_EQ(maoTeste5.getCartas(), teste1);
+TEST_CASE("Testando setters e getters"){
+	Mao mao;
+	std::vector<Carta> cartas = {Carta(Copas,Q), Carta(Paus, QUATRO)};
+	CHECK_NOTHROW_MESSAGE(mao.setCartas(cartas), "Set cartas OK!");
+	CHECK_EQ(mao.getCartas(), cartas);
 }

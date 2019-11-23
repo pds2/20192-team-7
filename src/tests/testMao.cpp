6801f24c989efe194431d3a4bc3104043e744c6a
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-11-23 14:16:40 -0300

    Correção do teste Mao

--- src/tests/testMao.cpp
+++ src/tests/testMao.cpp
@@ -4,16 +4,17 @@
 using namespace poker;
 
 TEST_CASE("Testando o construtor de Mao"){
+
 	Carta cartaTeste1(Paus, Q);
 	Carta cartaTeste2(Copas, Q);
 	Carta cartaTeste3(Ouros, Q);
-	Carta cartaTeste4(Espadas, Q;
+	Carta cartaTeste4(Espadas, Q);
 	Carta cartaTeste5(Paus, QUATRO);
 	Carta cartaTeste6(Copas, SETE);
 
-	CHECK_NOTHROW(maoTeste1(cartaTeste1, cartaTeste2));
-	CHECK_NOTHROWS(maoTeste3(cartaTeste3, cartaTeste4));
-	CHECK_NOTHROWS(maoTeste4(cartaTeste5, cartaTeste6));
+	CHECK_NOTHROW(Mao(cartaTeste1, cartaTeste2));
+	CHECK_NOTHROW(Mao(cartaTeste3, cartaTeste4));
+	CHECK_NOTHROW(Mao(cartaTeste5, cartaTeste6));
 }
 
 TEST_CASE("Testando getCartas"){
@@ -21,8 +22,8 @@ TEST_CASE("Testando getCartas"){
 	std::vector<Carta> teste1;
 	Carta cartaTeste7(Copas, Q);
 	Carta cartaTeste8(Paus, Q);
-	teste1.pushback(cartaTeste7);
-	teste1.pushback(cartaTeste8);
+	teste1.push_back(cartaTeste7);
+	teste1.push_back(cartaTeste8);
 	Mao maoTeste5(cartaTeste7, cartaTeste8);
 
 	CHECK_EQ(maoTeste5.getCartas(), teste1);

a5fca3d35deb8c7343ad8307cabf53e162490b2e
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-11-23 14:34:33 -0300

    Correção no testJogadorHumano

--- src/tests/testJogadorHumano.cpp
+++ src/tests/testJogadorHumano.cpp
@@ -6,6 +6,13 @@ using namespace poker;
 TEST_CASE("Testando o construtor de Jogador Humano"){
     Pote* pote;
     Mesa* mesa;
+    CHECK_NOTHROW_MESSAGE(JogadorHumano("NOME", 2000, pote, mesa), "1º Construtor de Jogador Humano OK!"); 
+    CHECK_NOTHROW_MESSAGE(JogadorHumano("NOME", pote, mesa), "2º Construtor de Jogador Humano OK!"); 
+}
+
+TEST_CASE("Testando o construtor de Jogador Humano"){
+    Pote* pote;
+    Mesa* mesa;
     CHECK_NOTHROW_MESSAGE(JogadorHumano("NOME", 2000, pote, mesa), "Construtor de Jogador Humano OK!");    
 }
 
@@ -18,25 +25,12 @@ TEST_CASE("Testando os getters de Jogador Humano"){
     CHECK_EQ(jogadorTest.getMesa(), mesaTest);
     CHECK_EQ(jogadorTest.getPote(), poteTest);
     CHECK_EQ(jogadorTest.getNumeroFichas, 2000);
-    CHECK_NOTHROW(jogadorTest.setMao(Mao(Carta(Copas, DOIS), Carta(Paus, TRES))));
-    CHECK_EQ(jogadorTest.getMao(), Mao(Carta(Copas, DOIS), Carta(Paus, TRES))));
+    Mao maoTest(Carta(Copas, DOIS), Carta(Paus, TRES));
+    Mao* maoPointer = &maoTest;
+    CHECK_NOTHROW(jogadorTest.setMao(maoPointer));
+    CHECK_EQ(jogadorTest.getMao(), Mao(Carta(Copas, DOIS), Carta(Paus, TRES)));
 }
 
-TEST_CASE("Testando o método Começar partida"){
-    Pote poteTest(40, 20, 20);
-    Mesa mesaTest(poteTest);
-    Pote* pote = &poteTest;
-    Mesa* mesa = &mesaTest;
-    std::vector<Jogador*>bots;
-    Jogador *Clinky = new Jogador("Clinky", 2000, poteTest, mesaTest);
-    Jogador *Adilson = new Jogador("Adilson", 2000, poteTest, mesaTest);
-    Jogador *Taylor = new Jogador("Taylor Swift", 2000, poteTest, mesaTest);
-    bots.push_back(Clinky);
-    bots.push_back(Adilson);
-    bots.push_back(Taylor);
-    JogadorHumano jogadorTest("NOME", 2000, pote, mesa);
-    CHECK_EQ(jogadorTest.comecarPartida(), bots);
-}
 
 TEST_CASE("Testando o método Finalizar partida"){
     Pote poteTest(40, 20, 20);
@@ -44,5 +38,12 @@ TEST_CASE("Testando o método Finalizar partida"){
     Pote* pote = &poteTest;
     Mesa* mesa = &mesaTest;
     JogadorHumano jogadorTest("NOME", 2000, pote, mesa);   
-    CHECK_NOTHROW_MESSAGE(jogadorTest.finalizarPartida(), "Finalizar partida OK!");
+    std::vector<Jogador*> bots;
+    Jogador *Clinky = new Jogador("Clinky", 2000, pote, mesa);
+    Jogador *Adilson = new Jogador("Adilson", 2000, pote, mesa);
+    Jogador *Taylor = new Jogador("Taylor Swift", 2000, pote, mesa);
+    bots.push_back(Clinky);
+    bots.push_back(Adilson);
+    bots.push_back(Taylor);
+    CHECK_NOTHROW_MESSAGE(jogadorTest.finalizarPartida(bots), "Finalizar partida OK!");
 }

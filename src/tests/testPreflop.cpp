eb0bf0ee153e74555af103e082e31620c874af42
lgstvo <lgustavo2001@gmail.com>
2019-11-24 15:48:09 -0300

    Implementação do testPreflop

--- src/tests/testPreflop.cpp
+++ src/tests/testPreflop.cpp
@@ -1,3 +1,49 @@
 #include <test/doctest.h>
 #include <classes/preflop.hpp>
 using namespace poker;
+
+TEST_CASE("Testando o construtor de PreFlop"){
+    CHECK_NOTHROW_MESSAGE( PreFlop(), "Construtor de PreFlop OK!" );
+}
+
+TEST_CASE("Testando o método distribuirCartas de PreFlop"){
+    PreFlop preflopTeste1;
+    Mesa* mesaPointer;
+    CHECK_NOTHROW_MESSAGE( preflopTeste1.distribuirCartas(mesaPointer), "Distribuição das cartas durante o PreFlop OK!" );
+}
+
+TEST_CASE("Testando o método de distribuirCartasJogadores de PreFlop"){
+    Mesa* mesa;
+    Pote* pote;
+    PreFlop preflopTeste3;
+
+    std::vector<Jogador> jogadorTeste;
+
+    Jogador jogadores[7] = {
+		Jogador("Clinky", pote, mesa),
+		Jogador("Adilson", pote, mesa),
+		Jogador("Taylor Swift", pote, mesa),
+		Jogador("Bryan", pote, mesa),
+		Jogador("Julio", pote, mesa),
+		Jogador("Ricky", pote, mesa),
+		JogadorHumano("Morty", pote, mesa),
+	};
+
+    for(int i = 0; i < 7; i++)
+        jogadorTeste.push_back(jogadores[i]);
+
+    CHECK_NOTHROW_MESSAGE(preflopTeste3.distribuirCartasJogadores(jogadorTeste), "Distribuição para os jogadores OK!");
+}
+
+TEST_CASE("Testando o método validarApostas de PreFlop"){
+    Mesa* mesaPointer1;
+    Pote* potePointer1;
+    int valorApostaTeste1 = 50, valorApostaTeste2 = 500, valorApostaTeste3 = 2000, valorApostaTeste4 = -50;
+    Jogador jogadorPointer( "NOME" , 200, potePointer1 , mesaPointer1);
+    PreFlop preflopTeste2;
+
+    CHECK_EQ(preflopTeste2.validarApostas(valorApostaTeste1, &jogadorPointer), 1);
+    CHECK_EQ(preflopTeste2.validarApostas(valorApostaTeste2, &jogadorPointer), 0);
+    CHECK_EQ(preflopTeste2.validarApostas(valorApostaTeste3, &jogadorPointer), 0);
+    CHECK_THROWS(preflopTeste2.validarApostas(valorApostaTeste4, &jogadorPointer));
+}

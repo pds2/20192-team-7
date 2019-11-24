94fde1500e136eeae957cb72a0b6039a37d0a5e7
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-11-24 12:18:38 -0300

    Modificações nos testes da classe Util

--- src/tests/testUtil.cpp
+++ src/tests/testUtil.cpp
@@ -14,3 +14,34 @@ TEST_CASE("Testando o método OrdenaCartas"){
     CHECK_NOTHROW_MESSAGE(cartas = util.OrdenaCartas(cartas), "Método de ordenação chamado OK!");
     CHECK_EQ(cartas, ordened);
 }
+
+TEST_CASE("Testando o método obter string símbolo"){
+    Util util;
+    CHECK_EQ(util.ObterStringSimbolo(DOIS), "Dois");
+    CHECK_EQ(util.ObterStringSimbolo(TRES), "Três");
+    CHECK_EQ(util.ObterStringSimbolo(QUATRO), "Quatro");
+    CHECK_EQ(util.ObterStringSimbolo(CINCO), "Cinco");
+    CHECK_EQ(util.ObterStringSimbolo(SEIS), "Seis");
+    CHECK_EQ(util.ObterStringSimbolo(SETE), "Sete");
+    CHECK_EQ(util.ObterStringSimbolo(OITO), "Oito");
+    CHECK_EQ(util.ObterStringSimbolo(NOVE), "Nove");
+    CHECK_EQ(util.ObterStringSimbolo(DEZ), "Dez");
+    CHECK_EQ(util.ObterStringSimbolo(J), "Valete");
+    CHECK_EQ(util.ObterStringSimbolo(Q), "Dama");
+    CHECK_EQ(util.ObterStringSimbolo(K), "Rei");
+    CHECK_EQ(util.ObterStringSimbolo(TRES), "Ás");
+}
+
+TEST_CASE("Testando o método obter string sequencia"){
+    Util util;
+    CHECK_EQ(util.ObterStringSequencia(HighCard), "Carta alta");
+    CHECK_EQ(util.ObterStringSequencia(Pair), "Par");
+    CHECK_EQ(util.ObterStringSequencia(TwoPair), "Duplo par");
+    CHECK_EQ(util.ObterStringSequencia(ThreeOfAKind), "Trio");
+    CHECK_EQ(util.ObterStringSequencia(Straight), "Straight");
+    CHECK_EQ(util.ObterStringSequencia(Flush), "Flush");
+    CHECK_EQ(util.ObterStringSequencia(FullHouse), "Full House");
+    CHECK_EQ(util.ObterStringSequencia(FourOfAKind), "Quadra");
+    CHECK_EQ(util.ObterStringSequencia(StraightFlush), "Straight Flush");
+    CHECK_EQ(util.ObterStringSequencia(RoyalFlush), "Royal Flush");
+}

73a824a95bc13e5a5f54fc919ee2ec28439d3417
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-11-24 11:21:00 -0300

    Correção nos testes de Estado Jogo

--- src/tests/testEstadoJogo.cpp
+++ src/tests/testEstadoJogo.cpp
@@ -6,5 +6,5 @@ TEST_CASE("Testando os construtores de EstadoJogo"){
     Baralho baralho;
     Baralho* baralhoPointer = &baralho;
     CHECK_NOTHROW_MESSAGE(EstadoJogo(), "Construtor nº1 OK!");
-    CHECK_NOTHROW_MESSAGE(EstadoJogo(baralhoPointer), "Construtor nº1 OK!");
+    CHECK_NOTHROW_MESSAGE(EstadoJogo(baralhoPointer), "Construtor nº2 OK!");
 }

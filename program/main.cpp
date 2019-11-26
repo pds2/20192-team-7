34f0bcd6964d81242d47e3cacd7def79d1d0492b
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-11-26 17:25:21 -0300

    Correção do bug na leitura do teclado

--- program/main.cpp
+++ program/main.cpp
@@ -5,11 +5,20 @@ int main() {
     poker::Dealer* dealer = new poker::Dealer();
 
     unsigned int numeroJogadores;
+    
     do {
+       
         std::cout << "Insira o número de jogadores rivais (entre 1 e 7):" << std::endl;
-        std::cin >> numeroJogadores;
-    } while (numeroJogadores > 7 || numeroJogadores < 1);
+        std::cout<< numeroJogadores<<std::endl;
 
+        if(!(std::cin >> numeroJogadores)){
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            std::cout<<"Digite um valor válido!"<<std::endl;
+        }
+
+    } while (numeroJogadores > 7 || numeroJogadores < 1);
+    
     dealer->iniciarJogo(numeroJogadores);
     return 0;
 }

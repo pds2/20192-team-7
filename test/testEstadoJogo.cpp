9ebc6372d00817c30a1e3035e2a27e96b9a3f697
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-12-02 19:18:13 -0300

    Correção no testEstadoJogo

--- test/testEstadoJogo.cpp
+++ test/testEstadoJogo.cpp
@@ -3,35 +3,23 @@
 using namespace poker;
 
 TEST_CASE("Testando os construtores de EstadoJogo"){
-    Baralho* baralhoPointer = new Baralho();
     CHECK_NOTHROW_MESSAGE(new EstadoJogo(), "Construtor nº1 OK!");
-    CHECK_NOTHROW_MESSAGE(new EstadoJogo(baralhoPointer), "Construtor nº2 OK!");
+    CHECK_NOTHROW_MESSAGE(new EstadoJogo(new Baralho), "Construtor nº2 OK!");
 }
 
-TEST_CASE("Testando verificarOpcoes do jogador"){
-    Baralho* baralhoPointer = new Baralho();
-    EstadoJogo estadojogo(baralhoPointer);
+TEST_CASE("Testando verificar Opcoes do jogador"){
+    EstadoJogo estadojogo(new Baralho);
     std::map<std::string, bool> mapaOpcoesJogador;
     mapaOpcoesJogador.insert(std::pair<std::string, bool>("check", false));
     mapaOpcoesJogador.insert(std::pair<std::string, bool>("apostar", false));
     mapaOpcoesJogador.insert(std::pair<std::string, bool>("desistir", false));
     mapaOpcoesJogador.insert(std::pair<std::string, bool>("pagar", false));
     mapaOpcoesJogador.insert(std::pair<std::string, bool>("aumentar", false));
-    std::map<std::string, bool> mapaOpcoesJogadorHumano;
-    mapaOpcoesJogadorHumano.insert(std::pair<std::string, bool>("check", true));
-    mapaOpcoesJogadorHumano.insert(std::pair<std::string, bool>("apostar", true));
-    mapaOpcoesJogadorHumano.insert(std::pair<std::string, bool>("desistir", true));
-    mapaOpcoesJogadorHumano.insert(std::pair<std::string, bool>("pagar", false));
-    mapaOpcoesJogadorHumano.insert(std::pair<std::string, bool>("aumentar", false));
     Pote poteTeste1(20, 20, 0);
     Pote poteTeste2(40, 20, 20);
     Pote* pote1 = &poteTeste1;
-    Pote* pote2 = &poteTeste2;
     Mesa* mesa = new Mesa();
     Jogador jogadorTeste("NOME", 2, pote1, mesa);
-    JogadorHumano jogadorHumanoTeste("HUMANO", 2000, pote2, mesa);
     Jogador* pointer1 = &jogadorTeste;
-    Jogador* pointer2 = &jogadorHumanoTeste;
     CHECK_EQ(estadojogo.verificarOpcoesJogador(pointer1, pote1), mapaOpcoesJogador);
-    CHECK_EQ(estadojogo.verificarOpcoesJogador(pointer2, pote2), mapaOpcoesJogadorHumano);
 }
